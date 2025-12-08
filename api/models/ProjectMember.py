949ce8628ddeaaf109cc124381f4d6a8e3538e79
Napasorn <tonnam.napasorn@gmail.com>
2025-12-09 00:16:19 +0700

    fix user model logic

--- api/models/ProjectMember.py
+++ api/models/ProjectMember.py
@@ -1,7 +1,7 @@
 from django.db import models
 import uuid
 from .Project import Project
-from .User import User
+from .BusinessUser import BusinessUser
 
 class ProjectMember(models.Model):
     STATUS_CHOICES = [
@@ -11,6 +11,6 @@ class ProjectMember(models.Model):
 
     project_member_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="members")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    user = models.ForeignKey(BusinessUser, on_delete=models.CASCADE, related_name="projects")
     hp = models.IntegerField(default=100)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="Alive")
--- api/models/UserReport.py
+++ api/models/UserReport.py
@@ -1,8 +1,8 @@
 from django.db import models
 from .Report import Report
-from .User import User
+from .BusinessUser import BusinessUser
 
 class UserReport(models.Model):
     report = models.ForeignKey(Report, on_delete=models.CASCADE, related_name="user_reports")
-    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="given_reports")
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_reports")
+    reviewer = models.ForeignKey(BusinessUser, on_delete=models.CASCADE, related_name="given_reports")
+    receiver = models.ForeignKey(BusinessUser, on_delete=models.CASCADE, related_name="received_reports")
