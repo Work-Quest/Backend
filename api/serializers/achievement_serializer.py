6f2dca0691c7dca3cd02f03329f8ed9005f14612
AtikarnKr <nanokwok3131@gmail.com>
2026-03-15 19:15:53 +0700

    Update Achievement sections

---
+++ api/serializers/achievement_serializer.py
@@ -0,0 +1,9 @@
+from api.services.achievement_service import (
+    compute_achievement_ids,
+    get_overall_achievement_ids_for_user,
+)
+
+__all__ = [
+    "compute_achievement_ids",
+    "get_overall_achievement_ids_for_user",
+]
