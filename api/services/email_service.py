39e67b0716001efe0925bcb8f7e766d2515b27be
Napasorn <tonnam.napasorn@gmail.com>
2026-03-22 18:00:36 +0700

    change to use promailer to send smtp

--- api/services/email_service.py
+++ api/services/email_service.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
+import requests
+import os
 
-
+url = "https://mailserver.automationlounge.com/api/v1/messages/send"
 class EmailService:
 
     def send_invite_email(request, email_metadata, project_metadata):
@@ -16,14 +18,21 @@ class EmailService:
             'project_name': project_metadata.get("project_name"),
             'url' : project_metadata.get("invite_url"),
         })
-        message = EmailMessage(
-                subject=email_metadata.get("subject"),
-                body=html_content,
-                from_email=getattr(settings, "DEFAULT_FROM_EMAIL", None),
-                to=email_metadata.get("recipients"),
-            )
-        message.content_subtype = "html"
-        message.send()
+
+        payload = {
+            "to": email_metadata.get("recipients"),
+            "subject": email_metadata.get("subject"),
+            "html": html_content
+        }
+        headers = {
+            "Authorization": f"Bearer {os.getenv('API_MAIL_KEY')}",
+            "Content-Type": "application/json",
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        if response.status_code != 200:
+            return False
 
 
         return True
